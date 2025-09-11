0c590719c87a912569501481b9ecc2fdf50f5699
Krish Sharma <krishsharma1903@gmail.com>
2025-09-11 23:32:53 +0530

    commit

---
+++ basics/areaOfCirlce.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+    int radius;
+    float area;
+    cout<<"Enter the radius of circle: ";
+    cin>>radius;
+    area=3.14*radius*radius;
+    cout<<"Area of the cirlce is: "<<area;
+    return 0;
+
+}
