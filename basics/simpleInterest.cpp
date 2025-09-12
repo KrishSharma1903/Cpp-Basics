c4e97ebec7c582deb9e36d3bca1d0d3cd4c60652
Krish Sharma <krishsharma1903@gmail.com>
2025-09-12 14:59:26 +0530

    commit

---
+++ basics/simpleInterest.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int p,t,r,SI;
+    cin>>p>>t>>r;
+    SI=(p*t*r)/100;
+    cout<<"Simple interest on this principal amount is "<<SI;
+
+    return 0;
+} 
