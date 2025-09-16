f723351bc04f874bca1cb6932b3666cac666448f
Krish Sharma <krishsharma1903@gmail.com>
2025-09-16 23:03:58 +0530

    commit

---
+++ Conditional/maxOfTwo.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x,y;
+    cin>>x>>y;
+    if(x>y)
+    {
+        cout<<x<<" is greater amongst the two.";
+    }
+    else
+    cout<<y<<" is greater amongst the two.";
+}
