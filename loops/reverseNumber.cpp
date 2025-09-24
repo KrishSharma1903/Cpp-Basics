9ab2f7f8c8093a33fb67fa21368b5b34186d7452
Krish Sharma <krishsharma1903@gmail.com>
2025-09-25 00:14:36 +0530

    commit

---
+++ loops/reverseNumber.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n,r,m,rev=0;
+    cout<<"Enter the number you want to revrse: ";
+    cin>>n;
+    m=n;
+    while(n>0)
+    {
+        r=n%10;
+        n=n/10;
+        rev=rev*10+r;
+    }
+    cout<<rev;
+}
